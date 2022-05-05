8d67972c1282aed056afa8a1a0108396e3d1c571
sandraanghel <1815743@student.uwtsd.ac.uk>
2022-05-05 12:40:18 +0100

    Create footer component

--- app/src/App.js
+++ app/src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import HomePage from './components/HomePage';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
     <>
     <Header />
     <HomePage />
+    <Footer />
     </>
     
   );
